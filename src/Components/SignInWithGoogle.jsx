530759064142f4cc15cf5511c83cb94fa2ca3996
bisre1921 <bisrattewodros3@gmail.com>
2024-03-17 20:20:03 +0300

    implement signIn with Google functionality

--- src/Components/SignInWithGoogle.jsx
+++ src/Components/SignInWithGoogle.jsx
@@ -1,8 +1,38 @@
+import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+import { doc, getDoc, serverTimestamp, setDoc } from "firebase/firestore";
 import { FcGoogle } from "react-icons/fc";
+import {toast} from "react-toastify";
+import { db } from "../firebase";
+import {Link , useNavigate} from "react-router-dom";
 
 const SignInWithGoogle = () => {
+  const navigate = useNavigate();
+  const handleSignInWithGoogle = async(event) => {
+    event.preventDefault();
+    try {
+      const auth = getAuth();
+      const provider = new GoogleAuthProvider();
+      const userCredential = await signInWithPopup(auth , provider);
+      const user = userCredential.user;
+      const docRef = doc(db , "users" , user.uid);
+      const docSnap = await getDoc(docRef);
+      if(!docSnap.exists()) {
+        await setDoc(docRef , {
+          name : user.displayName , 
+          email : user.email , 
+          timestamp : serverTimestamp()
+        });
+      };
+      navigate("/");
+    } catch (error) {
+      toast.error("Could not authorize with Google. Please try again.")
+    }
+  }
   return (
-      <button className="w-full bg-zinc-900 py-2 rounded hover:bg-zinc-700 transition duration-150 flex justify-center items-center gap-2">
+      <button 
+        className="w-full bg-zinc-900 py-2 rounded hover:bg-zinc-700 transition duration-150 flex justify-center items-center gap-2"
+        onClick={handleSignInWithGoogle}
+      >
         <FcGoogle className="text-2xl" />
         Continue with Google
       </button>
