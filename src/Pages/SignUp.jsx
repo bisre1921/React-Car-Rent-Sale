a2d083c0a939075ae589710d10d866d548134a61
bisre1921 <bisrattewodros3@gmail.com>
2024-03-17 16:05:04 +0300

    implement show password functionality for password input

--- src/Pages/SignUp.jsx
+++ src/Pages/SignUp.jsx
@@ -7,6 +7,8 @@ import { getAuth, createUserWithEmailAndPassword, updateProfile } from "firebase
 import { db } from "../firebase";
 import { doc, serverTimestamp, setDoc } from "firebase/firestore";
 import {toast} from "react-toastify";
+import { IoEye } from "react-icons/io5";
+import { IoEyeOff } from "react-icons/io5";
 
 const SignUp = () => {
 
@@ -15,6 +17,8 @@ const SignUp = () => {
     email : "" , 
     password : ""
   });
+
+  const [showPassword , setShowPassword] = useState(false);
 
   const {name , email , password} = formData;
 
@@ -78,14 +82,28 @@ const SignUp = () => {
               onChange={handleFormInputChange}
               className="w-full rounded px-2 py-4 mb-6 font-bold text-xl text-black"
             />
-            <input 
-              type="password" 
-              placeholder="password..."
-              value={password}
-              id="password"
-              onChange={handleFormInputChange}
-              className="w-full rounded px-2 py-4 mb-6 font-bold text-xl text-black"
-            />
+            <div className="relative">
+              <input 
+                type={showPassword ? "text" : "password"} 
+                placeholder="password..."
+                value={password}
+                id="password"
+                onChange={handleFormInputChange}
+                className="w-full rounded px-2 py-4 mb-6 font-bold text-xl text-black"
+              />
+              {showPassword ? (
+                  <IoEyeOff  
+                    className="absolute top-5 right-2 text-black text-xl cursor-pointer" 
+                    onClick={() => setShowPassword((prevState) => !prevState)}
+                  />
+              ) : (
+                  <IoEye 
+                    className="absolute top-5 right-2 text-black text-xl cursor-pointer" 
+                    onClick={() => setShowPassword((prevState) => !prevState)}
+                  />
+              )}
+              
+            </div>
             <div className="flex flex-col lg:flex-row justify-between mb-6">
                 <p>have an account?
                   <Link to="/sign-in" element={<SignIn />} className="ml-2 text-amber-700 font-bold text-xl hover:text-amber-500 transition duration-150">
