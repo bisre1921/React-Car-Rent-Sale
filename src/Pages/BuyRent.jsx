6e6f1a889ad8d90ecbe9e4cc199c912117beabf0
bisre1921 <bisrattewodros3@gmail.com>
2024-03-22 16:22:37 +0300

    create Rent page and implement its functionality

--- src/Pages/BuyRent.jsx
+++ src/Pages/BuyRent.jsx
@@ -8,6 +8,7 @@ import { Link } from "react-router-dom";
 const BuyRent = () => {
   //const [carListings , setCarListings] = useState(null);
   const [saleListings , setSaleListings] = useState(null);
+  const [rentListings , setRentListings] = useState(null);
   const [loading , setLoading] = useState(true);
   useEffect(() => {
     const fetchCarListing = async () => {
@@ -32,6 +33,30 @@ const BuyRent = () => {
     };
     fetchCarListing();
   });
+
+  useEffect(() => {
+    const fetchCarListing = async () => {
+       try {
+        const carListingsRef = collection(db , "sales");
+        const q = query(carListingsRef , where("type" , "==" , "rent") , orderBy("timestamp" , "desc") , limit(3));
+        const querySnap = await getDocs(q);
+        const carListings = [];
+        querySnap.forEach((doc) => {
+          return (
+            carListings.push({
+              id : doc.id , 
+              data : doc.data()
+            })
+          )
+        });
+        setRentListings(carListings);
+        setLoading(false);
+       } catch (error) {
+        console.log(error)
+       }
+    };
+    fetchCarListing();
+  });
   
   return (
     <div className="text-white max-w-7xl mx-auto my-10">
@@ -40,7 +65,7 @@ const BuyRent = () => {
           Buy OR Rent
         </h1>
       </div>
-      <div>
+      <div className="mb-16">
         <div className="mb-4">
           <h1 className="">
             You can Select the car which you want to buy
@@ -57,6 +82,29 @@ const BuyRent = () => {
                         key={saleListing.id}
                         id={saleListing.id}
                         carListing={saleListing.data}
+                      />
+                    )
+                })}
+            </div>
+          )}
+      </div>
+      <div>
+        <div className="mb-4">
+          <h1 className="">
+            You can Select the car which you want to Rent
+          </h1>
+          <Link to="/rent">
+            Show More Cars to Rent
+          </Link>
+        </div>
+          {!loading && rentListings?.length > 0 && (
+            <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-3 xl:grid-cols-3 gap-8">
+                {rentListings.map((rentListing) => {
+                    return (
+                      <CarListingItem
+                        key={rentListing.id}
+                        id={rentListing.id}
+                        carListing={rentListing.data}
                       />
                     )
                 })}
